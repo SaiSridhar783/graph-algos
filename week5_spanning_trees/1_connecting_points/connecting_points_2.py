d04126ed29a14ae12f3f5ad330880ecc824ae24b
Sai Sridhar Akula <sridhar.akula001@gmail.com>
2021-12-23 20:03:31 +0530

    Prims

--- week5_spanning_trees/1_connecting_points/connecting_points_2.py
+++ week5_spanning_trees/1_connecting_points/connecting_points_2.py
@@ -1,5 +1,6 @@
 # Uses python3
-# Using Kruskal's algorithm
+# Using Prim's algorithm
+from collections import defaultdict
 import sys
 import math
 from heapq import *
@@ -9,45 +10,49 @@ def euclidean_distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
+def minKey(key, mstSet, V):
+    minNum = sys.maxsize
+
+    for v in range(V):
+        if key[v] < minNum and mstSet[v] == False:
+            minNum = key[v]
+            min_index = v
+
+    return min_index
+
+
 def minimum_distance(x, y):
-    result = 0
-    # write your code here
-    heap = []
     V = len(x)
+    # Key values used to pick minimum weight edge in cut
+    key = [sys.maxsize] * V
+    parent = [None] * V  # Array to store constructed MST
+    # Make key 0 so that this vertex is picked as first vertex
+    key[0] = 0
+    mstSet = [False] * V
+
+    parent[0] = -1
+
+    maps = {node: f"{x[node]},{y[node]}" for node in range(V)}
+    graph = defaultdict(dict)
+    for i in range(V):
+        for j in range(V):
+            distance = euclidean_distance(x[i], y[i], x[j], y[j])
+            graph[f"{x[i]},{y[i]}"][f"{x[j]},{y[j]}"] = distance
 
     for i in range(V):
-        for j in range(i+1, V):
-            distance = euclidean_distance(x[i], y[i], x[j], y[j])
-            heappush(heap, (distance, f"{x[i]},{y[i]}", f"{x[j]},{y[j]}"))
+        u = minKey(key, mstSet, V)
+        mstSet[u] = True
 
-    visited = [set([f"{x[i]},{y[i]}"]) for i in range(V)]
-    ans = []
-    c = 0
-    while heap:
-        if c == V-1:
-            break
-        distance, point1, point2 = heappop(heap)
-        for disjoint in visited:
-            if point1 in disjoint and point2 in disjoint:
-                break
+        for v in range(V):
+            if graph[maps[u]][maps[v]] > 0 and mstSet[v] == False and key[v] > graph[maps[u]][maps[v]]:
+                key[v] = graph[maps[u]][maps[v]]
+                parent[v] = u
 
-        else:
-            for i in visited:
-                if point1 in i:
-                    i.add(point2)
-                elif point2 in i:
-                    i.add(point1)
-
-            result += distance
-
-            ans.append((distance, point1, point2))
-            c += 1
-
-    return result
+    return sum(key)
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read
+    #input = sys.stdin.read
     data = list(map(int, input().split()))
     n = data[0]
     x = data[1::2]
